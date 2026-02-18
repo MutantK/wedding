4a4b934f77127e9b99c859db4e005af3c56079d2
MutantK <kayatang@outlook.com>
2026-02-18 10:44:08 -0500

    added css and javascript

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+// main.js
+// Keep your JavaScript organised by grouping related functionality together
+
+// ================================
+// DOM READY
+// ================================
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Page loaded');
+
+  // Your code goes here
+});
